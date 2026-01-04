f27e8cc84560a8602eb5f019d8ab88b10ad08398
noshenko98 <noshenko98@gmail.com>
2026-01-04 17:12:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,10 +8,9 @@ def cache(func: Callable) -> Callable:
         key = (args, tuple(sorted(kwargs.items())))
         if save_result.get(key, "no_save") != "no_save" :
             print("Getting from cache")
-            return save_result[key]
 
         else:
             save_result[key] = func(*args, **kwargs)
             print("Calculating new result")
-            return save_result[key]
+        return save_result[key]
     return wrapper
