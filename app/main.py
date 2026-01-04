449b8967ba3f6b75d2f06a7237cb40f1fb006630
noshenko98 <noshenko98@gmail.com>
2026-01-04 16:52:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    save_result = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = tuple([f"{key} - {values}" for key, values in kwargs.items()])
+        if save_result.get(args, "no_save") != "no_save" :
+            print("Getting from cache")
+            return save_result[args]
+        elif save_result.get(key, "no_save") != "no_save" :
+            print("Getting from cache")
+            return save_result[key]
+        elif len(args) != 0:
+            save_result[args] = func(*args)
+            print("Calculating new result")
+            return save_result[args]
+        else:
+            save_result[key] = func(**kwargs)
+            print("Calculating new result")
+            return save_result[key]
+    return wrapper
